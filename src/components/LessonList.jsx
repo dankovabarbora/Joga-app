acc1bac849f89ae14823ceedbbd16601987e9b34
Barbora Daňková <bara.danek@gmail.com>
2021-06-05 16:19:07 +0200

    LessonDetail - vypis dat

--- src/components/LessonList.jsx
+++ src/components/LessonList.jsx
@@ -1,32 +1,31 @@
 import React from 'react';
 import './LessonList.css';
-import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link,
+  useParams,
+} from 'react-router-dom';
+import data from '../data.json';
 
 export const LessonList = () => {
   return (
     <>
       <h2 className="results__heading">Výsledky hledání</h2>
       <div className="results-intro">
-        <Link to="/lesson/:id">
-          <img className="results__img" src="assets/lotos.svg" alt="" />
-          <span>10.30</span> <span>Jógovna</span> <span>8 míst</span>
-          <span>Power jóga</span>
-        </Link>
-        <Link to="/lesson/:id">
-          <img className="results__img" src="assets/lotos.svg" alt="" />
-          <span>10.30</span> <span>Jógovna</span> <span>8 míst</span>
-          <span>Power jóga</span>
-        </Link>
-        <Link to="/lesson/:id">
-          <img className="results__img" src="assets/lotos.svg" alt="" />
-          <span>10.30</span> <span>Jógovna</span> <span>8 míst</span>
-          <span>Power jóga</span>
-        </Link>
-        <Link to="/lesson/:id">
-          <img className="results__img" src="assets/lotos.svg" alt="" />
-          <span>10.30</span> <span>Jógovna</span> <span>8 míst</span>
-          <span>Power jóga</span>
-        </Link>
+        {data.Lekce.map((lekce) => {
+          const time = new Date(lekce.time);
+          const studio = data.Studio.find((s) => s.id === lekce.studioId);
+          return (
+            <Link to="/lesson/:id">
+              <img className="results__img" src="assets/lotos.svg" alt="" />
+              <span>{` ${time.getHours()}: ${time.getMinutes()}`}</span>{' '}
+              <span>{studio.name}</span> <span>{lekce.occupancy}</span>
+              <span>{lekce.title}</span>
+            </Link>
+          );
+        })}
       </div>
     </>
   );
