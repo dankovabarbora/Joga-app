ad46ef8ded9eda82edcc92cb3eb5494973987d07
Šárka Princová Maroušková <sarka.marouskova@gmail.com>
2021-06-06 15:21:30 +0200

    Stylování LessonDetail

--- src/components/LessonDetail.jsx
+++ src/components/LessonDetail.jsx
@@ -20,27 +20,64 @@ export const LessonDetail = () => {
 
   return (
     <section className="oneResult">
-      
-        <h1 className="oneResult__heading">
-          <span>{` ${time.getHours()}:${time.getMinutes()}`} </span>
-          <span>{studio.name} </span>
-          <span>{lekce.title}</span>
-        </h1>
-        <div className="oneResult__div">
+      <h1 className="oneResult__heading">
+        <span>{` ${time.getHours()}:${time.getMinutes()}`} </span>
+        <span>{studio.name} </span>
+        <span>{lekce.title}</span>
+      </h1>
+      <div className="oneResult__div">
         <div className="oneResult__details">
           <ul>
             {/*<li>Název lekce: {lekce.title}</li>*/}
             <li className="oneResult__description">{lekce.description}</li>
             <hr></hr>
-            <li className="oneResult__capacity">Kapacita: {lekce.occupancy==="full" ? "Kapacita této lekce byla naplněna." : "Na této lekci jsou stále volná místa."}</li>
-            <li className="oneResult__difficulty">Náročnost: {lekce.difficulty} </li>
-            <li className="oneResult__length">Délka lekce (v hodinách): {lekce.length} </li>
+            <li className="oneResult__capacity">
+              Kapacita:{' '}
+              {lekce.occupancy === 'full'
+                ? 'Kapacita této lekce byla naplněna.'
+                : 'Na této lekci jsou stále volná místa.'}
+            </li>
+            <li className="oneResult__difficulty">
+              Náročnost: {lekce.difficulty}{' '}
+            </li>
+            <li className="oneResult__length">
+              Délka lekce (v hodinách): {lekce.length}{' '}
+            </li>
             <li className="oneResult__teacher">Lektor: {lekce.teacher}</li>
             <li className="oneResult__price">Cena: {lekce.price}</li>
+
             <div className="oneResult__contacts">
-            <li>Kontakt na studio: {`${studio.email}, ${studio.phone}`}</li>
-            <li>Webové stránky studia: {studio.website}</li>
-            <li>Rezervační sytém: {lekce.reservation}</li>
+              <li className="oneResult__phone">
+                <img
+                  className="oneResult__icons"
+                  src="/assets/phone.svg"
+                  alt="ikona telefon"
+                ></img>
+                <div className="oneResult__phone-text">{studio.phone}</div>
+              </li>
+              <div className="oneResult__links">
+                <li>
+                  <a href={studio.email}>
+                    <img
+                      className="oneResult__icons"
+                      src="/assets/mail.svg"
+                      alt="ikona email"
+                    ></img>
+                  </a>
+                </li>
+                <li>
+                  <a href={studio.website}>
+                    <img
+                      className="oneResult__icons"
+                      src="/assets/ikonka.svg"
+                      alt="ikona webové stránky"
+                    ></img>
+                  </a>
+                </li>
+              </div>
+              <a href={lekce.reservation}>
+                <button className="button__reservation">Rezervujte</button>
+              </a>
             </div>
           </ul>
         </div>
@@ -51,7 +88,7 @@ export const LessonDetail = () => {
             })}
           </div>
         </div>
-        
+
         {/* <button>
           <Link to="/lesson-list">Zpět na výsledky hledání</Link>
         </button> */}
