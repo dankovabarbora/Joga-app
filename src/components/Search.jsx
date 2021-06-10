1c58d6f12299e927626e0a3ebf2272bb1d40b3ec
Šárka Princová Maroušková <sarka.marouskova@gmail.com>
2021-06-10 15:24:05 +0200

    Stylování vyhledávacího formuláře (nefunguje podmíněné zobrazování, doladit responzivitu

--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -6,8 +6,14 @@ import { Map } from './Map';
 
 export const Search = (props) => {
   const [limit, setLimit] = useState(5);
+  const [openedFilter, setOpenedFilter] = useState(false);
+
   const loadMore = () => {
     setLimit(Number.MAX_SAFE_INTEGER);
+  };
+
+  const handleClick = () => {
+    setOpenedFilter(!openedFilter);
   };
 
   return (
@@ -17,120 +23,130 @@ export const Search = (props) => {
 
         <form id="search__form" className="search__form" action="">
           <div className="search__fields">
-            <label>
-              Městská část
-              <select
-                value={props.filter.location || ''}
-                onChange={(event) => {
-                  props.setFilter({
-                    ...props.filter,
-                    location: event.target.value,
-                  });
-                }}
-                name="district"
-              >
-                <option value="">Všechny městské části</option>
-                {data.map((location, index) => {
-                  return (
-                    <option
-                      key={index}
-                      value={`${location.latitude},${location.longitude}`}
-                    >
-                      {location.label}
-                    </option>
-                  );
-                })}
-              </select>
-            </label>
-            <br />
-            <label>
-              Den v týdnu
-              <select
-                value={props.filter.date || ''}
-                onChange={(event) => {
-                  props.setFilter({
-                    ...props.filter,
-                    date: event.target.value,
-                  });
-                }}
-              >
-                <option value="">Zvolte den...</option>
-                <option value="1">Pondělí</option>
-                <option value="2">Úterý</option>
-                <option value="3">Středa</option>
-                <option value="4">Čtvrtek</option>
-                <option value="5">Pátek</option>
-                <option value="6">Sobota</option>
-                <option value="7">Neděle</option>
-              </select>
-            </label>
-            <br />
+            <div className="search__fields-basic">
+              <label className="select-district">
+                Městská část
+                <select
+                  value={props.filter.location || ''}
+                  onChange={(event) => {
+                    props.setFilter({
+                      ...props.filter,
+                      location: event.target.value,
+                    });
+                  }}
+                  name="district"
+                >
+                  <option value="">Všechny městské části</option>
+                  {data.map((location, index) => {
+                    return (
+                      <option
+                        key={index}
+                        value={`${location.latitude},${location.longitude}`}
+                      >
+                        {location.label}
+                      </option>
+                    );
+                  })}
+                </select>
+              </label>
 
-            <label className="checkbox">
-              Pouze dostupné
-              <input
-                onChange={() => {
-                  props.setFilter({
-                    ...props.filter,
-                    available: !props.filter.available,
-                  });
-                }}
-                className="checkbox__input"
-                type="checkbox"
-                name=""
-                id=""
-                defaultChecked={props.filter.available}
-              />
-            </label>
-            {/*<button className="btn__advanced">Rozšířené hledání</button>*/}
+              <label className="select-day">
+                Den v týdnu
+                <select
+                  value={props.filter.date || ''}
+                  onChange={(event) => {
+                    props.setFilter({
+                      ...props.filter,
+                      date: event.target.value,
+                    });
+                  }}
+                >
+                  <option value="">Zvolte den...</option>
+                  <option value="1">Pondělí</option>
+                  <option value="2">Úterý</option>
+                  <option value="3">Středa</option>
+                  <option value="4">Čtvrtek</option>
+                  <option value="5">Pátek</option>
+                  <option value="6">Sobota</option>
+                  <option value="7">Neděle</option>
+                </select>
+              </label>
 
-            <label>
-              Výběr podle studia
-              <select
-                className="select__studio"
-                value={props.filter.studio || ''}
-                onChange={(event) => {
-                  props.setFilter({
-                    ...props.filter,
-                    studio: event.target.value,
-                  });
-                }}
-                name="studio__select"
-              >
-                <option value="">Všechna studia</option>
-                <option value="yogaart">YOGA &amp; Art</option>
-                <option value="karma-yoga">Studio Karma Yoga</option>
-                <option value="yogame">YOGAME</option>
-                <option value="jogovna">Jógovna</option>
-                <option value="jemny-svet">Jemný svět</option>
-                <option value="joga-letna">Jóga Letná</option>
-                <option value="joga-krymska">Jóga Krymská</option>
-                <option value="yoga-daily-life">
-                  Jóga v denním životě Praha 2
-                </option>
-                <option value="yoga-karlin">YogaKarlin</option>
-                <option value="aloha-joga">Aloha joga</option>
-              </select>
-            </label>
-            <br></br>
-            <label>
-              Náročnost
-              <select
-                value={props.filter.level || ''}
-                onChange={(event) => {
-                  props.setFilter({
-                    ...props.filter,
-                    level: event.target.value,
-                  });
-                }}
-              >
-                <option value="">Zvolte obtížnost...</option>
-                <option value="*">*</option>
-                <option value="**">**</option>
-                <option value="***">***</option>
-                <option value="****">****</option>
-              </select>
-            </label>
+              <label className="checkbox">
+                Pouze dostupné
+                <input
+                  onChange={() => {
+                    props.setFilter({
+                      ...props.filter,
+                      available: !props.filter.available,
+                    });
+                  }}
+                  className="checkbox__input"
+                  type="checkbox"
+                  name=""
+                  id=""
+                  defaultChecked={props.filter.available}
+                />
+              </label>
+            </div>
+            <button onClick={handleClick} className="btn__advanced">
+              Rozšířené hledání
+            </button>
+            <div
+              className={
+                openedFilter === false
+                  ? 'search__fields-advanced hidden'
+                  : 'search__fields-advanced'
+              }
+            >
+              <label className="select-studio">
+                Výběr podle studia
+                <select
+                  className="select__studio"
+                  value={props.filter.studio || ''}
+                  onChange={(event) => {
+                    props.setFilter({
+                      ...props.filter,
+                      studio: event.target.value,
+                    });
+                  }}
+                  name="studio__select"
+                >
+                  <option value="">Všechna studia</option>
+                  <option value="yogaart">YOGA &amp; Art</option>
+                  <option value="karma-yoga">Studio Karma Yoga</option>
+                  <option value="yogame">YOGAME</option>
+                  <option value="jogovna">Jógovna</option>
+                  <option value="jemny-svet">Jemný svět</option>
+                  <option value="joga-letna">Jóga Letná</option>
+                  <option value="joga-krymska">Jóga Krymská</option>
+                  <option value="yoga-daily-life">
+                    Jóga v denním životě Praha 2
+                  </option>
+                  <option value="yoga-karlin">YogaKarlin</option>
+                  <option value="aloha-joga">Aloha joga</option>
+                </select>
+              </label>
+
+              <label className="select-level">
+                Náročnost
+                <select
+                  value={props.filter.level || ''}
+                  onChange={(event) => {
+                    props.setFilter({
+                      ...props.filter,
+                      level: event.target.value,
+                    });
+                  }}
+                >
+                  <option value="">Zvolte obtížnost...</option>
+                  <option value="*">*</option>
+                  <option value="**">**</option>
+                  <option value="***">***</option>
+                  <option value="****">****</option>
+                </select>
+              </label>
+            </div>
           </div>
         </form>
       </div>
